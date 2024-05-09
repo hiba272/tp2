3828e49c26a78b0ac00bab5bab49f3f36646f3e1
hiba272 <hibachebbi78@gmail.com>
2024-05-09 23:40:47 +0200

    first commit tp2

---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
+export const routes: Routes = [
+    {path:'', component:CarteVisiteComponent}
+];
---
+++ src/app/carte-visite/carte-visite.component.ts
@@ -0,0 +1,24 @@
+import { Component,OnInit  } from '@angular/core';
+import { CarteVisite } from './carte-visite.model';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-carte-visite',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './carte-visite.component.html',
+  styleUrl: './carte-visite.component.css'
+})
+export class CarteVisiteComponent  {
+
+carteVisite: CarteVisite = new CarteVisite();
+constructor() { }
+gOnInit(): void {
+}
+
+onSubmit(): void {
+  
+  console.log(this.carteVisite);
+  }
+  
+
+}
